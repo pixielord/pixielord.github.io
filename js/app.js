6978eded54b9ac67866183d16fb06913610eb3ce
melvinstanly <2melvinstanly@gmail.com>
2021-02-14 22:14:32 +0530

    app.js recommit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 (function () {
- //  var worker = 'sw.js';
- //  var page = window.location.pathname;
- //  page = page.replace("/prayer-book/templates/","");
- //  if( page == 'prayers/' || page == 'holymass/'){
- //    worker = '../../sw.js';
- //  }
+  var worker = 'sw.js';
+  var page = window.location.pathname;
+  page = page.replace("/prayer-book/templates/","");
+  if( page == 'prayers/' || page == 'holymass/'){
+    worker = '../../sw.js';
+  }
 
- //  if ('serviceWorker' in navigator) {
- //    navigator.serviceWorker.register(worker)
- //    .then(function(registration) {
- //      console.log('Registration successful, scope is:', registration.scope);
- //    })
- //    .catch(function(error) {
- //      console.log('Service worker registration failed, error:', error);
- //    });
- //  }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(worker)
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
 
- // 	if ("serviceWorker" in navigator) {
- //  		window.addEventListener("load", function() {
- //    	navigator.serviceWorker
- //      		.register(worker)
- //      		.then(res => console.log("service worker registered"))
- //      		.catch(err => console.log("service worker not registered", err))
- //  		})
-	// }
+ 	if ("serviceWorker" in navigator) {
+  		window.addEventListener("load", function() {
+    	navigator.serviceWorker
+      		.register(worker)
+      		.then(res => console.log("service worker registered"))
+      		.catch(err => console.log("service worker not registered", err))
+  		})
+	}
 })();
