dd689112af3a0b51b9f133bb544d2cdffde3e40a
melvinstanly <2melvinstanly@gmail.com>
2021-02-23 13:12:09 +0530

    console error

--- js/app.js
+++ js/app.js
@@ -3,20 +3,12 @@
   
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(worker)
-    .then(function(registration) {
-      //console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      //console.log('Service worker registration failed, error:', error);
-    });
   }
 
  	if ("serviceWorker" in navigator) {
   		window.addEventListener("load", function() {
     	navigator.serviceWorker
       		.register(worker)
-      		.then(res => //console.log("service worker registered"))
-      		.catch(err => //console.log("service worker not registered", err))
   		})
 	}
 
